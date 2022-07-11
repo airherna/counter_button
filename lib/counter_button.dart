27e8f5fdc98270c94c4d56d7d60bf814b7780bdd
airherna <101186485+airherna@users.noreply.github.com>
2022-07-11 10:49:16 +0000

    Update counter_button.dart

--- lib/counter_button.dart
+++ lib/counter_button.dart
@@ -54,7 +54,6 @@ class _AnimatedCounterState extends State<CounterButton> {
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-        border: Border.all(width: 0.5),
         borderRadius: BorderRadius.circular(4.0),
       ),
       child: ClipRRect(
